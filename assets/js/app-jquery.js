03824b5fe8c16ea48d414929fe04ccf8b69b6768
gitlet <11thdim@gmail.com>
2017-04-08 04:19:13 +0530

    Updating, add more elements and animations

--- assets/js/app-jquery.js
+++ assets/js/app-jquery.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
   // alert('hello');
-  // $("body .wrapper .subsForm #subsBtn").on("click", function() {
-  //   $(this).toggleClass("wobble");
+  $("body .wrapper .subsForm #subsBtn").on("click", function() {
+    $(this).toggleClass("wobble");
+  });
+
+  // $("body .wrapper .subsForm #subsBtn").mousedown(function() {
+  //   $(this).css("transform", "scale(0.8, 0.8)");
+  //   // $(this).css("transition", "transform ease-in-out 2s");
+  // });
+  // $("body .wrapper .subsForm #subsBtn").mouseup(function() {
+  //   $(this).css("transform", "scale(1, 1)");
+  //   // $(this).css("transition", "transform ease-in-out 2s");
   // });
 
-  $("body .wrapper .subsForm #subsBtn").mousedown(function() {
-    $(this).css("transform", "scale(0.8, 0.8)", "transition", "transform ease-in-out 1s");
+  // CLASS FOR INPUT_SEARCH - ONCLICK INVOKE:
+  $("body #scaling-elems .wrapper #input_search").click(function() {
+    $(this).toggleClass("scale_width");
   });
-  $("body .wrapper .subsForm #subsBtn").mouseup(function() {
-    $(this).css("transform", "scale(1, 1)", "transition", "transform ease-in-out 1s");
+
+  // VERTICALLY SLIDING PANELS:
+  $("body #vertical_sliders .content span").click(function() {
+    $("body #vertical_sliders .content p").slideToggle();
   });
 
 });
